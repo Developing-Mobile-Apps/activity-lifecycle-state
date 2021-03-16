20a40e00e88159488902bcf6c9b865b00d4fb243
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-16 21:17:59 +0545

    FEAT: Add new layout for homework
    Replace the old layout and create new layout for homework. Also implement countUp method for button_count onClick handler

--- app/src/main/java/com/example/activityLifeCycleAndState/MainActivity.java
+++ app/src/main/java/com/example/activityLifeCycleAndState/MainActivity.java
@@ -1,73 +1,26 @@
 package com.example.activityLifeCycleAndState;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.twoactivities.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private int count;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // initialize all the view variables
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
-
-        // restore the state
-        if (savedInstanceState != null) {
-            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
-            if (isVisible) {
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
+        mShowCount = findViewById(R.id.show_count);
     }
 
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button Clicked");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+    public void countUp(View view) {
+        ++count;
+        mShowCount.setText("" + count);
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true);
-            outState.putString("reply_text", mReplyTextView.getText().toString());
-        }
-    }
-
 }
